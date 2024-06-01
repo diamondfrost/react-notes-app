8d4241ec14f2ff86360de66c8f05e589c3022492
Kat Chua <chua.katrina.ang@gmail.com>
2024-06-01 13:21:04 +0800

    Update to routes

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,15 +1,7 @@
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import { styled } from "@mui/material/styles";
 
-import HomePage from '../Components/Pages/HomePage';
-import About from '../Components/Pages/About';
-import CalendarPage from '../Components/Pages/CalendarPage';
-import AnnualReport from '../Components/Pages/AnnualReport';
-import Teams from '../Components/Pages/Teams';
-import Blogs from '../Components/Pages/Blogs';
-import SignUp from '../Components/Pages/SignUp';
 import NotesPage from "../Components/Pages/NotesPage";
-import SignIn from "../Components/Pages/SignIn";
 
 const HomeRoundedIconItem = styled(HomeRoundedIcon)({
     cursor: 'pointer',
